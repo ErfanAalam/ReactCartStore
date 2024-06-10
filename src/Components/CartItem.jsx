c9243bc751b2409d240b9110a75e2b6aa39997de
ErfanAalam <erfankhansiwani@gmail.com>
2024-06-10 15:20:01 +0530

    roundoff values

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -7,7 +7,7 @@ import { cartContext } from './CartProvider';
 
 const CartItem = () => {
 
-    const { products, handleRemove, totalPrice, handleDecrement, handleIncrement, handleClearCart, totalAmount } = useContext(cartContext)
+    const { products, handleRemove,  handleDecrement, handleIncrement, handleClearCart, totalAmount } = useContext(cartContext)
 
     return (
         <div className=''>
@@ -19,7 +19,7 @@ const CartItem = () => {
                             <img src={product.img} width={"430px"} height={"200px"} alt="hello" />
                             <div className=' flex flex-col justify-center items-center'>
                                 <h1>{product.title}</h1>
-                                <p>{product.price}</p>
+                                <p>{Math.ceil(product.price)}</p>
                                 <button onClick={() => { handleRemove(product.id) }} className='border-2 p-2 bg-gray-400'>Remove item</button>
                             </div>
                             <div className='flex flex-col justify-evenly'>
@@ -32,8 +32,7 @@ const CartItem = () => {
                 }
                 <hr className='mx-10 mb-10 border-2 border-black' />
                 <div className='flex justify-between px-48'>
-                    <h1 className='mx-20 font-700 text-[30px] '>Grand Total : {totalAmount}</h1>
-                    <h1 className='text-[34px] mx-96'>{totalPrice}</h1>
+                    <h1 className='mx-20 font-700 text-[30px] '>Grand Total : {Math.ceil(totalAmount)}</h1>
                     <button onClick={()=> handleClearCart()} className='border-2 p-4 bg-gray-400'>Clear Cart</button>
                 </div>
             </div>
